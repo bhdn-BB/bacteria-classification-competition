c3b824d75b86d887b6ff45441d1e617d41b6b2b8
Bohdan Boklah <b.boklag@gmail.com>
2025-08-10 01:29:25 +0300

    add augmentation

---
+++ preprocessing/augmentation.py
@@ -0,0 +1,31 @@
+import albumentations as A
+from albumentations import ToTensorV2
+from global_config import (
+    MEAN_IMAGENET,
+    STD_IMAGENET,
+    SIZE_IMAGENET,
+)
+
+train_transform = A.Compose([
+    A.RandomResizedCrop(
+        size=SIZE_IMAGENET,
+        scale=(0.75, 1.0),
+        ratio=(0.75, 1.0)
+    ),
+    A.HorizontalFlip(p=0.5),
+    A.Rotate(limit=180, p=0.5),
+    A.Normalize(
+        mean=MEAN_IMAGENET,
+        std=STD_IMAGENET,
+    ),
+    ToTensorV2(),
+])
+
+val_transform = A.Compose([
+    A.Resize(*SIZE_IMAGENET),
+    A.Normalize(
+        mean=MEAN_IMAGENET,
+        std=STD_IMAGENET,
+    ),
+    ToTensorV2(),
+])
