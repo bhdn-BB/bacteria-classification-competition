cc05bdb2c9e54c1d57985a69361b6cf3eb22e1f6
Bohdan Boklah <b.boklag@gmail.com>
2025-08-12 23:16:50 +0300

    remove color balancing from dataset class

--- dataset/protein_dataset.py
+++ dataset/protein_dataset.py
@@ -1,20 +1,28 @@
 import os
-
 from albumentations import ToTensorV2
-
 from global_config import NUM_CLASSES, COLORS_CHANNEL
 import cv2
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+from preprocessing.utils.balanced_color_channels import balance_color_channel
+
 
 class ProteinDataset(Dataset):
 
-    def __init__(self, df, image_dir, name_target_col, transform=None):
+    def __init__(
+            self,
+            df,
+            image_dir,
+            name_target_col,
+            transform=None,
+            # balance_channel=None,
+    ):
         self.df = df
         self.name_target_col = name_target_col
         self.image_dir = image_dir
         self.transform = transform
+        # self.balance_channel = balance_channel
 
     def __len__(self):
         return len(self.df)
@@ -34,11 +42,10 @@ class ProteinDataset(Dataset):
             channels.append(img)
 
         image = np.stack(channels, axis=-1).astype(np.float32)
-
+        # if self.balance_channel:
+        #     image = balance_color_channel(image, target_channel=self.balance_channel)
         if self.transform:
-            augmented = self.transform(image=image)             #convert to TensorV2
-            image = augmented['image']
+            image = self.transform(image=image)['image']
         else:
-            # image = torch.tensor(np.transpose(image, (2, 0, 1)), dtype=torch.float32)
             image = ToTensorV2()(image=image)["image"]
         return image, label_one_hot
