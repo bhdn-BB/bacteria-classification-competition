72f1df63d409d5499545b6de5cb7df91cc0a282f
Bohdan Boklah <b.boklag@gmail.com>
2025-08-13 00:53:50 +0300

    add configs for DinoV2

--- models/configs/best_dino_v2.py
+++ models/configs/best_dino_v2.py
@@ -1,7 +1,11 @@
-
+from global_config import WORKING_DIR
 
 MODEL = 'facebook/dinov2-base-imagenet1k-1-layer'
-EPOCHS = 50
+EPOCHS = 15
 BATCH_SIZE = 128
 LR = 3E-4
 LOG_INTERVAL = 10
+WEIGHT_DECAY = 3E-3
+OUTPUT_DIR = WORKING_DIR / "dino2-finetuned"
+metrics = "f1_macro"
+GRADIENT_ACCUMULATION_STEPS = 16
