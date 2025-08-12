72febcf3f208971eda8d588cf905c192435948e4
Bohdan Boklah <b.boklag@gmail.com>
2025-08-12 23:25:18 +0300

    improve wandb callback

--- callbacks/wandb_checkpoint_callback.py
+++ callbacks/wandb_checkpoint_callback.py
@@ -1,41 +1,54 @@
 import wandb
 import torch
 import os
-from typing import Dict, Any
+from transformers import TrainerCallback
 
-class WandbCheckpointCallback:
 
-    def __init__(self, save_dir: str = "checkpoints"):
+class WandbCheckpointCallback(TrainerCallback):
+    def __init__(
+            self,
+            save_dir: str = "checkpoints",
+            metric_name="eval_f1_macro",
+            greater_is_better=True
+    ):
         self.save_dir = save_dir
         os.makedirs(self.save_dir, exist_ok=True)
+        self.metric_name = metric_name
+        self.greater_is_better = greater_is_better
+        self.best_metric = None
+        self.best_model_path = os.path.join(self.save_dir, "model-best.pt")
         self.last_model_path = os.path.join(self.save_dir, "model-last.pt")
 
-    def __call__(self, metrics: Dict[str, Any]) -> None:
-        epoch = metrics["epoch"]
-        model = metrics["model"]
-        is_best = metrics["is_best"]
-        eval_metric = metrics.get("eval_metric", None)
-
+    def on_epoch_end(self, args, state, control, **kwargs):
+        trainer = kwargs.get("trainer")
+        model = trainer.model
+        metrics = state.log_history[-1]
+        epoch = int(metrics.get("epoch", state.epoch))
+        current_metric = metrics.get(self.metric_name)
         torch.save(model.state_dict(), self.last_model_path)
-        last_artifact = wandb.Artifact("model-last", type="model")
+        last_artifact = wandb.Artifact(f"model-last", type="model")
         last_artifact.add_file(self.last_model_path)
         last_artifact.metadata.update({
             "epoch": epoch,
-            "eval_metric": eval_metric,
-            "is_best": is_best,
+            self.metric_name: current_metric,
+            "is_best": False,
         })
         wandb.log_artifact(last_artifact)
-        print(f"[W&B] Logged LAST checkpoint (epoch {epoch})")
-
-        if is_best:
-            best_model_path = os.path.join(self.save_dir, "model-best.pt")
-            torch.save(model.state_dict(), best_model_path)
-            best_artifact = wandb.Artifact("model-best", type="model")
-            best_artifact.add_file(best_model_path)
+        print(f"[W&B] Logged LAST checkpoint (epoch {epoch}, {self.metric_name}={current_metric})")
+        if (self.best_metric is None) or \
+           (self.greater_is_better and current_metric > self.best_metric) or \
+           (not self.greater_is_better and current_metric < self.best_metric):
+            self.best_metric = current_metric
+            torch.save(model.state_dict(), self.best_model_path)
+            best_artifact = wandb.Artifact(f"model-best", type="model")
+            best_artifact.add_file(self.best_model_path)
             best_artifact.metadata.update({
                 "epoch": epoch,
-                "eval_metric": eval_metric,
-                "is_best": is_best,
+                self.metric_name: current_metric,
+                "is_best": True,
             })
             wandb.log_artifact(best_artifact)
-            print(f"[W&B] Logged BEST checkpoint (epoch {epoch})")
+            print(f"[W&B] Logged BEST checkpoint (epoch {epoch}, {self.metric_name}={current_metric})")
+
+    def on_init_end(self, args, state, control, **kwargs):
+        pass
